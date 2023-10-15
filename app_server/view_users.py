7d9f1544d40e3bcbfd1ed11dd363a15a3bec62bb
Igor Ter-Pogosyan <igor-526@yandex.ru>
2023-10-15 19:16:54 +0300

    Fixed bug

--- app_server/view_users.py
+++ app_server/view_users.py
@@ -1,14 +1,18 @@
 from flask import jsonify, request
+from sqlalchemy import extract
+
 import config
 from auth import authorizate
 from models import db, User, Time, City, Base
+from datetime import date
 
 
 async def get_user():
     user = authorizate(request.headers.get("Authorization"))
     async with db.with_bind(config.POSTGRES_URI):
         query = await User.query.where(User.id == user['id']).gino.first()
-        time = await Time.query.where(Time.driver == user["id"]).gino.all()
+        query = await (Time.query.where(Time.driver == user["id"]).where(extract('month', Time.start) == date.month)
+                       .where(extract('year', Time.start) == date.year).gino.all())
         city = await City.query.where(City.id == user["city"]).gino.first()
         base = await Base.query.where(Base.id == user["base"]).gino.first()
     hours_worked = 0
